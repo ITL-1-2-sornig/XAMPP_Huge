131ab8afff50a60e6b465538312400ed33149814
sornig <thomas.sornig@knapp.com>
2026-05-11 18:08:00 +0200

    Unique-Key für email in der users-Tabelle entfernt

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Projekt für ITL_1_2
 
 Um die User Tabelle manuel anzupassen
 ``` sql
-ALTER TABLE `users` CHANGE `user_email` `user_email` VARCHAR(254) CHARACTER SET utf8 COLLATE utf8_unicode_ci COMMENT 'user\'s email, unique';
+ALTER TABLE `users` CHANGE `user_email` `user_email` VARCHAR(254) CHARACTER SET utf8 COLLATE utf8_unicode_ci COMMENT 'user\'s email';
+ALTER TABLE `users` DROP INDEX `user_email`;
 ```
--- huge-3.3.1/application/_installation/02-create-table-users.sql
+++ huge-3.3.1/application/_installation/02-create-table-users.sql
@@ -19,8 +19,7 @@ CREATE TABLE IF NOT EXISTS `huge`.`users` (
  `user_password_reset_timestamp` bigint(20) DEFAULT NULL COMMENT 'timestamp of the password reset request',
  `user_provider_type` text COLLATE utf8_unicode_ci,
  PRIMARY KEY (`user_id`),
- UNIQUE KEY `user_name` (`user_name`),
- UNIQUE KEY `user_email` (`user_email`)
+ UNIQUE KEY `user_name` (`user_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data';
 
 INSERT INTO `huge`.`users` (`user_id`, `session_id`, `user_name`, `user_password_hash`, `user_email`, `user_active`, `user_deleted`, `user_account_type`,
