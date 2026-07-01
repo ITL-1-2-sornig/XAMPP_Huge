c41c6391af0fb9765212315f110fb9b062ddf460
ThomasSornig <thomas.sornig@knapp.com>
2026-07-01 00:21:04 -0700

    Recaptcha implementiert

--- huge-3.3.1/application/view/register/index.php
+++ huge-3.3.1/application/view/register/index.php
@@ -1,3 +1,4 @@
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <div class="container">
 
     <!-- echo out the system feedback (error and success messages) -->
@@ -15,24 +16,9 @@
             <input type="text" name="user_email_repeat" placeholder="repeat email address (to prevent typos)" />
             <input type="password" name="user_password_new" pattern=".{6,}" placeholder="Password (6+ characters)" required autocomplete="off" />
             <input type="password" name="user_password_repeat" pattern=".{6,}" required placeholder="Repeat your password" autocomplete="off" />
-
-            <!-- show the captcha by calling the login/showCaptcha-method in the src attribute of the img tag
-            <img id="captcha" src="<?php echo Config::get('URL'); ?>register/showCaptcha" />
-            <input type="text" name="captcha" placeholder="Please enter above characters" required />
-
-            <!-- quick & dirty captcha reloader -->
-            <!--<a href="#" style="display: block; font-size: 11px; margin: 5px 0 15px 0; text-align: center"
-               onclick="document.getElementById('captcha').src = '<?php echo Config::get('URL'); ?>register/showCaptcha?' + Math.random(); return false">Reload Captcha</a> -->
+            <div class="g-recaptcha" data-sitekey="<?= Config::get("RECAPTCH_SITE_KEY")?>"></div>
 
             <input type="submit" value="Register" />
         </form>
     </div>
-</div>
-<div class="container">
-    <p style="display: block; font-size: 11px; color: #999;">
-        Please note: This captcha will be generated when the img tag requests the captcha-generation
-        (= a real image) from YOURURL/register/showcaptcha. As this is a client-side triggered request, a
-        $_SESSION["captcha"] dump will not show the captcha characters. The captcha generation
-        happens AFTER the request that generates THIS page has been finished.
-    </p>
 </div>
